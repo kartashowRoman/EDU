aadecdc872966ead4d2ea73e1204e28d7a5aebc0
kartashowRoman <roman.kartashow@yandex.ru>
2021-03-19 05:35:02 +0300

    too late

--- HW2/A/A.py
+++ HW2/A/A.py
@@ -1,3 +1,11 @@
 def solution(arr):
     ### YOUR CODE ###
-    return
+    count = 1
+    res = []
+    for i in range(len(arr)):
+    	if arr[i] == arr[i-1]:
+    		count+=1
+    	else:
+    		res.append(count)
+    		count = 1
+    return max(res, default = 1)
--- HW2/B/B.py
+++ HW2/B/B.py
@@ -1,3 +1,19 @@
 def solution(x):
     ### YOUR CODE ###
-    return
+    #first step
+    x_del = list(x[0:]) 
+    del x_del[::3]
+    x_del = ''.join(x_del)
+    x_whole = x[0]+x_del
+
+    #second
+    
+    first_index = x_whole.find('h')
+    last_index = x_whole.rfind('h')
+    x_whole_upper = x_whole[first_index:last_index].replace('h', 'H')
+    x_whole = x_whole[:first_index] + x_whole_upper + x_whole[last_index:]
+
+    #third
+    x_whole = x_whole.replace('1', 'one')
+
+    return x_whole
