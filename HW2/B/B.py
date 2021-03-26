23408a23e6aa23851b7f1bef6b9a8c29d1b94e1d
kartashowRoman <roman.kartashow@yandex.ru>
2021-03-26 13:26:02 +0300

    26.03

--- HW2/B/B.py
+++ HW2/B/B.py
@@ -4,16 +4,27 @@ def solution(x):
     x_del = list(x[0:]) 
     del x_del[::3]
     x_del = ''.join(x_del)
-    x_whole = x[0]+x_del
+    x_whole = x[0]+ x_del
 
     #second
     
-    first_index = x_whole.find('h')
+    
     last_index = x_whole.rfind('h')
-    x_whole_upper = x_whole[first_index:last_index].replace('h', 'H')
-    x_whole = x_whole[:first_index] + x_whole_upper + x_whole[last_index:]
+    x_whole_upper = x_whole[:last_index].replace('h', 'H')
+    x_whole = x_whole_upper + x_whole[last_index:]
+    x_whole = x_whole.replace('H', 'h', 1)
+
 
     #third
     x_whole = x_whole.replace('1', 'one')
 
+    
+
     return x_whole
+
+'''
+Expected:
+bl9kittcrzk0qqqwrdrdwdrfdwtqroc05qbzkli0vkHb3frfqb3fl3wzr90ifbvtz005Htovv03wzwq9lfctcwfonekciwid5o3vtqz3dwHlv3illwv0roHkitonebHobwobqik50wbo5fkrwwonedzonefqorrbwtoit3q03ck5cvko5lzq3ktronedoHtbizvlHrkvdbrviowlo3wkdk9zonevoneldwwone9ic9q0ti3kwc5qH93wH5ofi9woneikord5oqlrzbiq5bH5vfvr0k9zzwiiltz33wrvH09lq3loctdb395zone5k5kdwoner09oneHoneo5vonebb5zvv00rblbv9vbt5onedHb0Honebczc53k5dcd9fwoor0klf3qonewonedz0Hl9btH9vltllrlqkivcwtwit9tzk5twtzonerdwonewbf5i9q9krwonef0wfcvilcvdqHbvodfoblllz9oneorwtzzov5vdht93zqf
+Got:
+bl9kittcrzk0qqqwrdrdwdrfdwtqroc05qbzkli0vkhb3frfqb3fl3wzr90ifbvtz005Htovv03wzwq9lfctcwfonekciwid5o3vtqz3dwHlv3illwv0roHkitonebHobwobqik50wbo5fkrwwonedzonefqorrbwtoit3q03ck5cvko5lzq3ktronedoHtbizvlHrkvdbrviowlo3wkdk9zonevoneldwwone9ic9q0ti3kwc5qH93wH5ofi9woneikord5oqlrzbiq5bH5vfvr0k9zzwiiltz33wrvH09lq3loctdb395zone5k5kdwoner09oneHoneo5vonebb5zvv00rblbv9vbt5onedHb0Honebczc53k5dcd9fwoor0klf3qonewonedz0Hl9btH9vltllrlqkivcwtwit9tzk5twtzonerdwonewbf5i9q9krwonef0wfcvilcvdqHbvodfoblllz9oneorwtzzov5vdht93zqf
+'''
--- HW2/C/C.py
+++ HW2/C/C.py
@@ -1,3 +1,35 @@
 def solution(arr):
     ### YOUR CODE ###
-    return
+    
+    res = []
+    if len(arr) == 0:
+    	return res
+    row_begin = 0
+    row_end = len(arr)-1
+    col_begin = 0
+    col_end = len(arr[0])-1
+
+    while row_begin <= row_end and col_begin <= col_end:
+        for i in range(col_begin, col_end+1):
+            res.append(arr[row_begin][i])
+        row_begin += 1
+
+        for i in range(row_begin, row_end+1):
+            res.append(arr[i][col_end])
+        col_end -= 1
+
+        if row_begin <= row_end:
+            for i in range(col_end, col_begin-1, -1):
+                res.append(arr[row_end][i])
+        row_end -= 1
+
+        if col_begin <= col_end:
+            for i in range(row_end, row_begin-1, -1):
+                res.append(arr[i][col_begin])
+        col_begin += 1
+ 
+    return res
+
+
+
+    
